3991faac38ff204ec9221169676818259b562143
Olivia Forbault <olivia.forbault@outlook.fr>
2023-02-23 11:10:12 +0100

    added recipe for 1 person

---
+++ recipes/script.js
@@ -0,0 +1,23 @@
+let factor = document.querySelector(".portion-number").innerHTML;
+
+let amount = document.querySelector(".amount").innerHTML;
+let unit = document.querySelector(".unit").innerHTML;
+let name = document.querySelector(".name").innerHTML;
+
+const hummus = function () {
+  let ingredient = function (amount, unit, name) {
+    let ingredientAmount = amount * factor;
+    if (ingredientAmount > 1) {
+      unit += "s";
+    }
+    console.log(`${ingredientAmount} ${unit} ${name}`);
+  };
+  ingredient(1, "can", "chickpeas");
+  ingredient(0.25, "cup", "tahini");
+  ingredient(0.25, "cup", "lemon juice");
+  ingredient(1, "clove", "garlic");
+  ingredient(2, "tablespoon", "olive oil");
+  ingredient(0.5, "teaspoon", "cumin");
+};
+
+hummus(factor);
