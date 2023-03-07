70b7e866ad40f787330c46deb2435aaf2cea859d
Olivia Forbault <olivia.forbault@outlook.fr>
2023-03-07 10:03:09 +0100

    changes to dynamic menu

--- art-gallery/script.js
+++ art-gallery/script.js
@@ -1,19 +1,19 @@
 let crossOpening = document.querySelector(".cross-opening");
 let crossClosing = document.querySelector(".cross-closing");
-
 let menu = document.querySelector(".menu");
 
 crossOpening.addEventListener("click", (event) => {
-  menu.style = "width: 450px; z-index: 20; transition: 2s";
+  menu.style = "width: 450px";
 });
 
 crossClosing.addEventListener("click", (event) => {
-  menu.style = "width: 0px; z-index: -10; transition: 2s";
+  menu.style = "width: 0px";
 });
 
 let responsiveMenu = document.querySelector(".responsive-menu");
 console.log(responsiveMenu);
 
 responsiveMenu.addEventListener("click", (event) => {
-  menu.style = "display: block; width: 100%; z-index: 11; height: 675px";
+  menu.style =
+    "display: block; width: auto; height: 675px; transition: 0.5s ease";
 });
